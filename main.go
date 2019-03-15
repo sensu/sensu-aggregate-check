14ffef3f84fa96467d44ae1eb9eff234a53d4194
Sean Porter <portertech@gmail.com>
2019-03-15 10:02:22 -0600

    auth and events fetching
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- main.go
+++ main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
-	//	"encoding/json"
+	"bytes"
+	"encoding/json"
 	"fmt"
-	//	"io/ioutil"
+	"io/ioutil"
+	"net/http"
 	"os"
 
-	//	"github.com/sensu/sensu-go/types"
+	"github.com/sensu/sensu-go/types"
 	"github.com/spf13/cobra"
 )
 
 var (
 	labels     string
 	namespaces string
-	stdin      *os.File
+	apiHost    string
+	apiPort    string
+	apiUser    string
+	apiPass    string
 )
+
+type Auth struct {
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresAt    int64  `json:"expires_at"`
+}
 
 func main() {
 	rootCmd := configureRootCommand()
@@ -43,6 +54,30 @@ func configureRootCommand() *cobra.Command {
 		"default",
 		"us-east-1,us-west-2")
 
+	cmd.Flags().StringVarP(&apiHost,
+		"api-host",
+		"H",
+		"127.0.0.1",
+		"sensu-backend.example.com")
+
+	cmd.Flags().StringVarP(&apiPort,
+		"api-port",
+		"p",
+		"8080",
+		"5555")
+
+	cmd.Flags().StringVarP(&apiUser,
+		"api-user",
+		"u",
+		"admin",
+		"ackbar")
+
+	cmd.Flags().StringVarP(&apiPass,
+		"api-pass",
+		"P",
+		"P@ssw0rd!",
+		"itsatrap")
+
 	_ = cmd.MarkFlagRequired("labels")
 
 	return cmd
@@ -57,8 +92,69 @@ func run(cmd *cobra.Command, args []string) error {
 	return evalAggregate()
 }
 
-func evalAggregate() error {
-	fmt.Printf("hello world: %s\n", labels)
+func authenticate() (Auth, error) {
+	var auth Auth
+	req, err := http.NewRequest(
+		"GET",
+		fmt.Sprintf("http://%s:%s/auth", apiHost, apiPort),
+		nil,
+	)
+	if err != nil {
+		return auth, err
+	}
 
-	return nil
+	req.SetBasicAuth(apiUser, apiPass)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return auth, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return auth, err
+	}
+
+	err = json.NewDecoder(bytes.NewReader(body)).Decode(&auth)
+
+	return auth, err
+}
+
+func getEvents(auth Auth, namespace string, labels string) ([]*types.Event, error) {
+	url := fmt.Sprintf("http://%s:%s/api/core/v2/namespaces/%s/events", apiHost, apiPort, namespace)
+	events := []*types.Event{}
+	req, err := http.NewRequest(
+		"GET",
+		url,
+		nil,
+	)
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", auth.AccessToken))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return events, err
+	}
+
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(b))
+
+	return events, nil
+}
+
+func evalAggregate() error {
+	auth, err := authenticate()
+
+	if err != nil {
+		return err
+	}
+
+	events, err := getEvents(auth, "default", "aggregate=foo")
+
+	fmt.Printf("hello world: %s\n", events)
+
+	return err
 }
